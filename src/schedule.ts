21a97779f5179db041d623e691433a6bc735e92d
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-09-20 20:47:44 -0700

    Add ViewMode and range classes and remove thing preventing me from committing

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,4 +1,4 @@
-export class Time {
+export class ScheduleTime {
     private readonly _totalMinutes: number;
 
     constructor(hours: number, minutes: number) {
@@ -18,9 +18,54 @@ export class Time {
     }
 }
 
+class ScheduleDate {
+  static now(): ScheduleDate {
+    return new ScheduleDate(new Date());
+  }
+
+  static fromString(dateString: string): ScheduleDate {
+    let splitDate = dateString.split("-");
+    return new ScheduleDate(new Date(parseInt(splitDate[0]), parseInt(splitDate[1]) - 1, parseInt(splitDate[2])));
+  }
+
+  private readonly date: Date;
+
+  private constructor(date: Date) {
+    this.date = date;
+  }
+
+  copy(): ScheduleDate {
+    return new ScheduleDate(new Date(this.date));
+  }
+
+  getDay() {
+    return this.date.getDay();
+  }
+
+  getDate() {
+    return this.date.getDate();
+  }
+
+  setDate(date: number) {
+    this.date.setDate(date);
+  }
+
+  getFullYear() {
+    return this.date.getFullYear();
+  }
+
+  getMonth() {
+    return this.date.getMonth();
+  }
+
+  toString() {
+    return [this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate()].join("-");
+  }
+}
+
 class Block {
-    startTime: Time;
-    endTime: Time;
+  startTime: ScheduleTime;
+  endTime: ScheduleTime;
     title: string;
     subtitle: string;
     location: string;
@@ -28,7 +73,7 @@ class Block {
 }
 
 abstract class Day {
-    date: Date;
+  date: ScheduleDate;
 }
 
 abstract class BlockDay extends Day {
@@ -56,8 +101,8 @@ class Utilities {
 }
 
 enum ViewMode {
-    Week,
-    Day,
+  Day = 1,
+  Week = 5,
 }
 
 class ScheduleManager {
@@ -71,13 +116,12 @@ class ScheduleManager {
         return calendarUUID;
     }
 
-    static getSeedDate(): Date {
+  static getSeedDate(): ScheduleDate {
         const dateString = Utilities.getUrlParam("date");
         if (dateString !== null) {
-            let splitDate = dateString.split("-");
-            return new Date(parseInt(splitDate[0]), parseInt(splitDate[1]) - 1, parseInt(splitDate[2]));
+          return ScheduleDate.fromString(dateString);
         } else {
-            return new Date();
+          return ScheduleDate.now();
         }
     }
 
@@ -95,16 +139,119 @@ class ScheduleManager {
     }
 }
 
+class ScheduleRange {
+  private readonly _currentDate: ScheduleDate;
+  private readonly _nextDate: ScheduleDate;
+  private readonly _previousDate: ScheduleDate;
+  private readonly _viewMode: ViewMode;
+
+  constructor(currentDate: ScheduleDate, viewMode: ViewMode) { // TODO: Clean up this mess
+    this._viewMode = viewMode;
+    this._currentDate = currentDate;
+    const thisMonday = ScheduleRange.getLastFriday(currentDate);
+    thisMonday.setDate(thisMonday.getDate() + 3);
+    if (viewMode === ViewMode.Week) {
+      // get the previous and next monday for the arrows TODO: Dont modify the dates inside getDatesForWeek so this order doesn't matter
+      const previousMonday = thisMonday.copy();
+      const nextMonday = thisMonday.copy();
+      previousMonday.setDate(thisMonday.getDate() - 7);
+      nextMonday.setDate(thisMonday.getDate() + 7);
+      this._nextDate = nextMonday;
+      this._previousDate = previousMonday;
+    } else if (viewMode === ViewMode.Day) {
+      let isWeekend = (date: ScheduleDate) => date.getDay() === 0 || date.getDay() === 6;
+      let startDate = isWeekend(currentDate) ? thisMonday : currentDate;
+
+      this._nextDate = startDate.copy();
+      this._previousDate = startDate.copy();
+      this._nextDate.setDate(startDate.getDate() + 1);
+      if (isWeekend(this._nextDate)) this._nextDate.setDate(startDate.getDate() + 3);
+      this._previousDate.setDate(startDate.getDate() - 1);
+      if (isWeekend(this._previousDate)) this._previousDate.setDate(startDate.getDate() - 3);
+    } else {
+      throw new Error("Unsupported ViewMode value!");
+    }
+  }
+
+  private static getLastFriday(date: ScheduleDate) {
+    let d = date.copy();
+    let day = d.getDay();
+    let diff = (day <= 5) ? (7 - 5 + day) : (day - 5);
+
+    d.setDate(d.getDate() - diff);
+    return d;
+  }
+
+  get nextDate(): ScheduleDate {
+    return this._nextDate;
+  }
+
+  get currentDate(): ScheduleDate {
+    return this._currentDate;
+  }
+
+  get previousDate(): ScheduleDate {
+    return this._previousDate;
+  }
+
+  get viewMode(): ViewMode {
+    return this._viewMode;
+  }
+}
+
+class ScheduleFactory {
+  private calendarUUID: string; // todo uuid type
+  private blocks: Block[];
+
+  constructor(calendarUUID: string) {
+    this.calendarUUID = calendarUUID;
+  }
+
+  addBlocksICS(ics: string) {
+
+  }
+}
+
 class CacheManager {
 
 }
 
 class ScheduleRenderer {
+  static updateLinks(caldendarUUID: string, range: ScheduleRange) {
+    $("#schedarea .sched").addClass(range.viewMode === ViewMode.Week ? "week" : "today");
+    // left/right arrows
+    let navigationArrows = $("td.arrows a");
+    navigationArrows.first().prop("href", "?date=" + range.previousDate.toString() + "&range=" + range + "&cal=" + caldendarUUID);
+    navigationArrows.last().prop("href", "?date=" + range.nextDate.toString() + "&range=" + range + "&cal=" + caldendarUUID);
 
+    // this week
+    $("#today a").prop("href", "?range=day" + "&cal=" + caldendarUUID);
+    $("#this-week a").prop("href", "?range=week" + "&cal=" + caldendarUUID);
+
+    // return to portal link
+    $("td.controls.links a").last().prop("href", "https://portals.veracross.com/catlin/student/student/daily-schedule?date=" + range.currentDate.toString());
+  }
 }
 
 window.addEventListener('load', () => {
     let calendarUUID = ScheduleManager.getCalendarUUID();
     let seedDate = ScheduleManager.getSeedDate();
-    let viewmode = ScheduleManager.getViewMode();
+  let viewMode = ScheduleManager.getViewMode();
+  let range = new ScheduleRange(seedDate, viewMode);
+  ScheduleRenderer.updateLinks(calendarUUID, range);
 });
+
+/*
+//Promise chain:
+load data
+    if load from cache if valid
+    else download and process cal data
+    save to cache
+render data
+
+//Cal data process:
+new block(event)
+map date -> blocks
+new day(blocks)
+
+ */
