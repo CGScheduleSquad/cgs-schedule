355d09d7c6704f76a97063cc3ce88788b98e3bab
Tristan Peng <pengt@catlin.edu>
2019-09-21 17:19:53 -0700

    Put inline js into separate file

---
+++ src/script.js
@@ -0,0 +1,32 @@
+const uuidFromWebcalLink = link => {
+  return link.match(
+    /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?=\.ics)/g
+  );
+};
+window.addEventListener('load', () => {
+  let forms = document.getElementsByClassName('needs-validation');
+  Array.from(forms).forEach(form => {
+    form.addEventListener(
+      'submit',
+      event => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          event.preventDefault();
+          event.stopPropagation();
+          $('#disclaimerModal').modal();
+        }
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+  $('.generate-link').click(() => {
+    localStorage.removeItem('scheduleEvents');
+    let url = `./schedule.html?cal=${ uuidFromWebcalLink(
+      $('#allClassesUrl').val()
+    ) }`;
+    window.open(url, '_blank');
+  });
+});
