9a28d09fef0b5485f4314d96980d6abd936d3395
Tristan Peng <pengt@catlin.edu>
2019-09-22 15:24:53 -0700

    Clean up some code

--- src/schedule/time/scheduleDate.ts
+++ src/schedule/time/scheduleDate.ts
@@ -1,52 +1,32 @@
-export class ScheduleDate {
-    static now(): ScheduleDate {
-        return new ScheduleDate(new Date());
-    }
+export default class ScheduleDate {
+  static now = (): ScheduleDate => new ScheduleDate(new Date());
 
-    static fromString(dateString: string): ScheduleDate {
-        let splitDate = dateString.split('-');
-        return new ScheduleDate(new Date(parseInt(splitDate[0]), parseInt(splitDate[1]) - 1, parseInt(splitDate[2])));
-    }
+  static fromString(dateString: string): ScheduleDate {
+    let splitDate = dateString.split('-');
+    return new ScheduleDate(new Date(parseInt(splitDate[0]), parseInt(splitDate[1]) - 1, parseInt(splitDate[2])));
+  }
 
-    static fromDate(date: Date): ScheduleDate {
-        return new ScheduleDate(date);
-    }
+  static fromDate = (date: Date): ScheduleDate => new ScheduleDate(date);
 
-    private readonly date: Date;
+  private readonly date: Date;
 
-    private constructor(date: Date) {
-        this.date = date;
-    }
+  private constructor(date: Date) {
+    this.date = date;
+  }
 
-    copy(): ScheduleDate {
-        return new ScheduleDate(new Date(this.date));
-    }
+  copy = (): ScheduleDate => new ScheduleDate(new Date(this.date));
 
-    getDay() {
-        return this.date.getDay();
-    }
+  getDay = (): number => this.date.getDay();
 
-    getDate() {
-        return this.date.getDate();
-    }
+  getDate = (): number => this.date.getDate();
 
-    setDate(date: number) {
-        this.date.setDate(date);
-    }
+  setDate = (date: number) => this.date.setDate(date);
 
-    getFullYear() {
-        return this.date.getFullYear();
-    }
+  getFullYear = (): number => this.date.getFullYear();
 
-    getMonth() {
-        return this.date.getMonth();
-    }
+  getMonth = (): number => this.date.getMonth();
 
-    toString() {
-        return [this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate()].join('-');
-    }
+  toString = (): string => [this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate()].join('-');
 
-    toJSON() {
-        return this.toString();
-    }
+  toJSON = (): string => this.toString();
 }
