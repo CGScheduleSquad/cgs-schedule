77de098b9ccd23288d7be3e20d606fefb1a7a3be
Tristan Peng <pengt@catlin.edu>
2019-09-23 15:16:22 -0700

    Clean up some code

--- src/schedule/main.ts
+++ src/schedule/main.ts
@@ -19,13 +19,8 @@ class ScheduleParamUtils {
 
     static getSeedDate(): ScheduleDate {
         const dateString = ScheduleParamUtils.getUrlParam('date');
-        if (dateString !== null) {
-            return ScheduleDate.fromString(dateString);
-        } else {
-            return ScheduleDate.now();
-        }
+        return dateString !== null ? ScheduleDate.fromString(dateString) : ScheduleDate.now();
     }
-
 
     static getViewMode(): ViewMode {
         let newRange = ScheduleParamUtils.getUrlParam('range');
@@ -39,30 +34,32 @@ class ScheduleParamUtils {
         }
     }
 
-    private static getUrlParam(key: string) {
-        return new URL(window.location.href).searchParams.get(key);
-    }
+    private static getUrlParam = (key: string) => new URL(window.location.href).searchParams.get(key);
 }
 
 class ScheduleCacheManager {
     public static readonly LOCAL_STORAGE_KEY = 'scheduleEvents';
 
-    static getSchedule(calendarUUID: string): Promise<any> {
-        if (localStorage === undefined) { // not supported
+    static async getSchedule(calendarUUID: string): Promise<any> {
+        if (localStorage === undefined) {
+            // not supported
             console.log('Local storage is not supported! Loading schedule...');
-            return this.reloadSchedulePromise(calendarUUID).then(jsonString => JSON.parse(jsonString));
+            const jsonString = await this.reloadSchedulePromise(calendarUUID);
+            return JSON.parse(jsonString);
         }
 
         let scheduleString = localStorage.getItem(ScheduleCacheManager.LOCAL_STORAGE_KEY);
         if (scheduleString === null) {
             console.log('Schedule cache does not exist! Loading schedule...');
-            return this.reloadSchedulePromise(calendarUUID).then(jsonString => JSON.parse(jsonString));
+            const jsonString_1 = await this.reloadSchedulePromise(calendarUUID);
+            return JSON.parse(jsonString_1);
         }
 
         let scheduleObject = JSON.parse(scheduleString);
         if (scheduleObject.versionNumber !== ScheduleAll.CURRENT_VERSION_NUMBER || scheduleObject.id !== calendarUUID) {
             console.log('Schedule cache is invalid! Loading schedule...');
-            return this.reloadSchedulePromise(calendarUUID).then(jsonString => JSON.parse(jsonString));
+            const jsonString_2 = await this.reloadSchedulePromise(calendarUUID);
+            return JSON.parse(jsonString_2);
         }
 
         if (new Date().getTime() - scheduleObject.creationTime > 1000 * 60 * 60 * 24) {
@@ -75,18 +72,28 @@ class ScheduleCacheManager {
     }
 
     private static reloadSchedulePromise(calendarUUID: string): Promise<string> {
-        return ScheduleBuilder.generateScheduleFromBlockSources(calendarUUID, new VeracrossICSRawBlockSource(calendarUUID))
-            .then((schedule: ScheduleAll) => {
-                let jsonString = JSON.stringify(schedule);
-                localStorage.setItem('scheduleEvents', jsonString);
-                console.log('Schedule reloaded from Veracross and saved to localStorage!');
-                return jsonString;
-            });
+        return ScheduleBuilder.generateScheduleFromBlockSources(
+            calendarUUID,
+            new VeracrossICSRawBlockSource(calendarUUID)
+        ).then((schedule: ScheduleAll) => {
+            let jsonString = JSON.stringify(schedule);
+            localStorage.setItem('scheduleEvents', jsonString);
+            console.log('Schedule reloaded from Veracross and saved to localStorage!');
+            return jsonString;
+        });
     }
 }
 
 const colorDict = {
-    0: '#C0C0C0', 1: '#FFCE51', 2: '#A67FB9', 3: '#E67326', 4: '#00ABBD', 5: '#AAC02C', 6: '#EF4957', 7: '#FF75F2', free: 'white'
+    0: '#C0C0C0',
+    1: '#FFCE51',
+    2: '#A67FB9',
+    3: '#E67326',
+    4: '#00ABBD',
+    5: '#AAC02C',
+    6: '#EF4957',
+    7: '#FF75F2',
+    free: 'white'
 };
 
 let calendarUUID = ScheduleParamUtils.getCalendarUUID();
@@ -94,64 +101,67 @@ let seedDate = ScheduleParamUtils.getSeedDate();
 let viewMode = ScheduleParamUtils.getViewMode();
 let range = new ScheduleRange(seedDate, viewMode);
 
-
 console.log('Start schedule program');
-Promise.all([ScheduleCacheManager.getSchedule(calendarUUID), new Promise(((resolve, reject) => {
-    window.addEventListener('DOMContentLoaded', () => {
-        resolve();
-    });
-}))]).then((things: any) => {
+Promise.all([
+    ScheduleCacheManager.getSchedule(calendarUUID),
+    new Promise(resolve => {
+        window.addEventListener('DOMContentLoaded', () => {
+            resolve();
+        });
+    })
+]).then((things: any) => {
     let schedule = things[0];
     console.log(schedule);
-    range.getDatesForWeek()
-        .forEach((date: ScheduleDate) => {
-            let rawDay = schedule.dayMap[date.toString()];
+    range.getDatesForWeek().forEach((date: ScheduleDate) => {
+        let rawDay = schedule.dayMap[date.toString()];
 
-            let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-            let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-            // append the header with a link to the veracross page
-            $('table.sched.main > tbody > tr:nth-child(1)').append(`
+        // append the header with a link to the veracross page
+        $('table.sched.main > tbody > tr:nth-child(1)').append(`
                 <td class="daylabel">
                   <a href="https://portals.veracross.com/catlin/student/student/daily-schedule?date=${date.toString()}">
                     <b>
-                      ${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate() + (rawDay === undefined || !rawDay.dayMeta ? '' : ` (${rawDay.dayMeta})`)}
+                      ${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate() +
+            (rawDay === undefined || !rawDay.dayMeta ? '' : ` (${rawDay.dayMeta})`)}
                     </b>
                   </a>
                 </td>
                 `);
 
-            if (rawDay === undefined) {
-                appendBlankSchedule('No Events', colorDict.free);
-                return;
-            }
+        if (rawDay === undefined) {
+            appendBlankSchedule('No Events', colorDict.free);
+            return;
+        }
 
-            switch (rawDay.type) {
-                case ScheduleDayType.TEXT:
-                    appendBlankSchedule('TODO', colorDict.free);
-                    break;
-                case ScheduleDayType.INLINE:
-                    appendInlineSchedule(rawDay, schedule.compressionList);
-                    break;
-                case ScheduleDayType.REGULAR:
-                    appendRegularSchedule(rawDay, schedule.compressionList);
-                    break;
-
-            }
-        });
+        switch (rawDay.type) {
+            case ScheduleDayType.TEXT:
+                appendBlankSchedule('TODO', colorDict.free);
+                break;
+            case ScheduleDayType.INLINE:
+                appendInlineSchedule(rawDay, schedule.compressionList);
+                break;
+            case ScheduleDayType.REGULAR:
+                appendRegularSchedule(rawDay, schedule.compressionList);
+                break;
+        }
+    });
     $('#schedarea').show();
 
     ScheduleRenderer.updateLinks(calendarUUID, range);
 });
 
-
-function appendBlankSchedule(text: string, bgcolor: string, link: string = '') { // TODO: Remove jquery dependency
-    return $('table.sched.main > tbody > tr:nth-child(2)').append(`<td rowspan="12" class="specialday" style="background: ${bgcolor};"><a ${link === '' ? '' : `href=${link}`} class="coursename">${text}</a></td>`);
+function appendBlankSchedule(text: string, bgcolor: string, link: string = '') {
+    // TODO: Remove jquery dependency
+    return $('table.sched.main > tbody > tr:nth-child(2)').append(
+        `<td rowspan="12" class="specialday" style="background: ${bgcolor};"><a ${
+            link === '' ? '' : `href=${link}`
+        } class="coursename">${text}</a></td>`
+    );
 }
 
-function appendInlineSchedule(rawDay: any, compressionList: Array<string>) {
-
-}
+function appendInlineSchedule(rawDay: any, compressionList: Array<string>) {}
 
 function appendRegularSchedule(rawDay: any, compressionList: Array<string>) {
     rawDay.blocks.forEach(block => {
@@ -163,31 +173,47 @@ function appendRegularSchedule(rawDay: any, compressionList: Array<string>) {
         let mins = block[5];
         let free = block[6];
 
-        let subtitle = location + (blockLabel === '' ? '' : ' - ' + (blockLabel.match(/\d(?![ Flex|X])/) !== null ? 'Blk ' : '') + blockLabel);
+        let subtitle =
+            location +
+            (blockLabel === ''
+                ? ''
+                : ' - ' + (blockLabel.match(/\d(?![ Flex|X])/) !== null ? 'Blk ' : '') + blockLabel);
 
         let smallBlock = title === subtitle || subtitle === '' || title === 'US C&C';
         let blockNumMatchAttempt = blockLabel.match(/\d(?![ Flex|X])/);
-        let bgcolor = blockNumMatchAttempt !== null ? colorDict[parseInt(blockNumMatchAttempt[0].slice(-1))] : ((free || subtitle.match(/Break/) != null || subtitle.match(/Lunch/) != null) ? colorDict.free : colorDict[0]);
+        let bgcolor =
+            blockNumMatchAttempt !== null
+                ? colorDict[parseInt(blockNumMatchAttempt[0].slice(-1))]
+                : free || subtitle.match(/Break/) != null || subtitle.match(/Lunch/) != null
+                ? colorDict.free
+                : colorDict[0];
 
-      let trElement = document.getElementById(`time-${normalTimeIndex + 1}`); // TODO: Get all elements and put them in an array, then use those instead of searching by ID
+        let trElement = document.getElementById(`time-${normalTimeIndex + 1}`); // TODO: Get all elements and put them in an array, then use those instead of searching by ID
         trElement.appendChild(generateBlockElement(rowSpan, mins, bgcolor, title, subtitle, !smallBlock));
         // $(`table.sched.main > tbody > tr:nth-child(${normalTimeIndex + 2})`).append(`<td rowspan="${rowSpan}" class="period mins${mins}" style="background: ${bgcolor};"><span class="coursename">${title}</span>${smallBlock ? '' : '<br>'}<span class="subtitle">${smallBlock ? '' : subtitle}</span><br></td>`);
     });
 }
 
-function generateBlockElement(rowSpan:number, mins:string, bgcolor:string, title:string, subtitle:string, newLine: boolean) {
-    let tableData = document.createElement("td");
-    tableData.setAttribute("rowspan", rowSpan);
-    tableData.setAttribute("class", `period mins${mins}`);
-    tableData.setAttribute("style", `background: ${bgcolor};`); // todo replace style with css class
-    let titleSpan = document.createElement("span");
-    titleSpan.setAttribute("class", "coursename");
+function generateBlockElement(
+    rowSpan: number,
+    mins: string,
+    bgcolor: string,
+    title: string,
+    subtitle: string,
+    newLine: boolean
+) {
+    let tableData = document.createElement('td');
+    tableData.setAttribute('rowspan', rowSpan);
+    tableData.setAttribute('class', `period mins${mins}`);
+    tableData.setAttribute('style', `background: ${bgcolor};`); // todo replace style with css class
+    let titleSpan = document.createElement('span');
+    titleSpan.setAttribute('class', 'coursename');
     titleSpan.appendChild(document.createTextNode(title));
-    let subtitleSpan = document.createElement("subtitle");
-    subtitleSpan.setAttribute("class", "subtitle");
+    let subtitleSpan = document.createElement('subtitle');
+    subtitleSpan.setAttribute('class', 'subtitle');
     subtitleSpan.appendChild(document.createTextNode(subtitle));
     tableData.appendChild(titleSpan);
-    if (newLine) tableData.appendChild(document.createElement("br"));
+    if (newLine) tableData.appendChild(document.createElement('br'));
     tableData.appendChild(subtitleSpan);
     return tableData;
 }
