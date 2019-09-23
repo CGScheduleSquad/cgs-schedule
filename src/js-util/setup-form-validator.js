b8b02887eefa7d3b3d29e4cfc7eae1e8a9228a69
Tristan Peng <pengt@catlin.edu>
2019-09-23 15:09:59 -0700

    Remove as much jQuery as possible

--- src/js-util/setup-form-validator.js
+++ src/js-util/setup-form-validator.js
@@ -1,32 +1,21 @@
-const uuidFromWebcalLink = link => {
-  return link.match(
-    /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?=\.ics)/g
-  );
-};
+const uuidFromWebcalLink = link =>
+    link.match(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?=\.ics)/g);
 window.addEventListener('load', () => {
-  let forms = document.getElementsByClassName('needs-validation');
-  Array.from(forms).forEach(form => {
-    form.addEventListener(
-      'submit',
-      event => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        } else {
-          event.preventDefault();
-          event.stopPropagation();
-          $('#disclaimerModal').modal();
-        }
-        form.classList.add('was-validated');
-      },
-      false
-    );
-  });
-  $('.generate-link').click(() => {
-    localStorage.removeItem('scheduleEvents');
-    let url = `./schedule.html?cal=${uuidFromWebcalLink(
-      $('#allClassesUrl').val()
-    )}`;
-    window.open(url, '_blank');
-  });
+    Array.from(document.getElementsByClassName('needs-validation')).forEach(form => {
+        form.addEventListener(
+            'submit',
+            event => {
+              event.preventDefault();
+              event.stopPropagation();
+                if (form.checkValidity()) $('#disclaimerModal').modal();
+                form.classList.add('was-validated');
+            },
+            false
+        );
+    });
+    document.getElementsByClassName('generate-link')[0].addEventListener('click', () => {
+      localStorage.removeItem('scheduleEvents');
+      let url = `./schedule.html?cal=${uuidFromWebcalLink(document.getElementById('allClassesUrl').value)}`;
+      window.open(url, '_blank');
+    });
 });
