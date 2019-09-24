8d4143c880dbf44ed25aee93aeb7aa6353c6e67b
Tristan Peng <pengt@catlin.edu>
2019-09-24 15:02:01 -0700

    Clean up JSONRawBlockSource

--- src/schedule/json/jsonRawBlockSource.ts
+++ src/schedule/json/jsonRawBlockSource.ts
@@ -2,36 +2,30 @@ import { RawBlockSource } from '../building/scheduleBuilder';
 import { RawBlock } from '../structure/scheduleBlock';
 import ScheduleDate from '../time/scheduleDate';
 import ScheduleTime from '../time/scheduleTime';
+// @ts-ignore
 import blocks from './blocks.json';
 
-export class JsonRawBlockSource implements RawBlockSource {
+export default class JSONRawBlockSource implements RawBlockSource {
 
-    constructor() {
-    }
+    constructor() {}
 
-    getBlocksPromise(): Promise<RawBlock[]> {
-        return new Promise<RawBlock[]>((resolve, reject) => {
-            let rawBlocks = [];
-            for (let date in blocks) {
-                blocks[date].forEach(block => {
-                    let startTime = ScheduleTime.fromDate(JsonRawBlockSource.parseVeracrossTime(block.startTime));
+    getBlocksPromise = (): Promise<RawBlock[]> => new Promise<RawBlock[]>(resolve => {
+        let rawBlocks: RawBlock[] = [];
+        for (let date in blocks) {
+            if (blocks.hasOwnProperty(date)) {
+                blocks[date].forEach((block: { startTime: string; title: string; }) => {
+                    let startTime = ScheduleTime.fromDate(JSONRawBlockSource.parseVeracrossTime(block.startTime));
                     if (startTime.hours === 11 && startTime.minutes === 25) startTime = new ScheduleTime(11, 20); // TODO: get rid of this
                     rawBlocks.push(new RawBlock(block.title, '', '', ScheduleDate.fromString(date), null, startTime, null));
                 });
             }
-            resolve(rawBlocks);
-        });
-    }
+        }
+        resolve(rawBlocks);
+    });
 
     private static parseVeracrossTime(timeString: string) {
-        let isPm = false;
-        if (timeString.includes('am')) {
-            timeString = timeString.replace(' am', '');
-        }
-        if (timeString.includes('pm')) {
-            timeString = timeString.replace(' pm', '');
-            isPm = true;
-        }
+        let isPm = timeString.includes('pm');
+        timeString = timeString.replace(/[ap]m/g, '');
         let splitString = timeString.split(':');
         return new Date(0, 0, 0, parseInt(splitString[0]) + (isPm && parseInt(splitString[0]) !== 12 ? 12 : 0), parseInt(splitString[1]));
     }
