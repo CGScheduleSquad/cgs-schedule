be1a2af68a02b0e98c03de639d1a06c369e64e0e
Tristan Peng <pengt@catlin.edu>
2019-09-25 11:30:58 -0700

    Remove jQuery from scheduleRenderer

--- src/schedule/rendering/scheduleRenderer.ts
+++ src/schedule/rendering/scheduleRenderer.ts
@@ -1,25 +1,25 @@
 import { ScheduleRange, ViewMode } from './scheduleRange';
 
 export class ScheduleRenderer {
-    static updateLinks(caldendarUUID: string, range: ScheduleRange) {
+    static updateLinks(calendarUUID: string, range: ScheduleRange) {
         let viewMode = range.viewMode === ViewMode.Day ? 'day' : 'week';
-        $('#schedarea .sched').addClass(range.viewMode === ViewMode.Week ? 'week' : 'today');
+        // @ts-ignore
+        let schedarea = document.getElementById('schedarea').firstElementChild;
+        // @ts-ignore
+        schedarea.setAttribute('class', `${schedarea.getAttribute('class')} ${range.viewMode === ViewMode.Week ? 'week' : 'today'}`);
         // left/right arrows
-        let navigationArrows = $('td.arrows a');
-        navigationArrows
-            .first()
-            .prop('href', '?date=' + range.previousDate.toString() + '&range=' + viewMode + '&cal=' + caldendarUUID);
-        navigationArrows
-            .last()
-            .prop('href', '?date=' + range.nextDate.toString() + '&range=' + viewMode + '&cal=' + caldendarUUID);
+        let navigationArrows = document.querySelectorAll('td.arrows a');
+        // @ts-ignore
+        navigationArrows[0].setAttribute('href', `?date=${range.previousDate.toString()}&range=${viewMode}&cal=${calendarUUID}`);
+        // @ts-ignore
+        navigationArrows[1].setAttribute('href', `?date=${range.nextDate.toString()}&range=${viewMode}&cal=${calendarUUID}`);
 
         // this week
-        $('#today a').prop('href', '?range=day' + '&cal=' + caldendarUUID);
-        $('#this-week a').prop('href', '?range=week' + '&cal=' + caldendarUUID);
-
-        // return to portal link
-        $('td.controls.links a')
-            .last()
-            .prop('href', 'https://portals.veracross.com/catlin/student/student/daily-schedule?date=' + range.startDate.toString());
+        // @ts-ignore
+        document.getElementById('today').firstElementChild.setAttribute('href', `?range=day&cal=${calendarUUID}`);
+        // @ts-ignore
+        document.getElementById('this-week').firstElementChild.setAttribute('href', `?range=week&cal=${calendarUUID}`);
+        // @ts-ignore
+        document.getElementById('my-portal').setAttribute('href', `https://portals.veracross.com/catlin/student/student/daily-schedule?date=${range.startDate.toString()}`);
     }
 }
