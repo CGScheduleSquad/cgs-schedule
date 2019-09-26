7fbb4bcc46998fca6606a1f42fffb22a7ae0adc4
Tristan Peng <pengt@catlin.edu>
2019-09-25 17:49:07 -0700

    Replace Bootstrap with Bulma

--- src/js-util/setup-form-validator.js
+++ src/js-util/setup-form-validator.js
@@ -1,18 +1,30 @@
 const uuidFromWebcalLink = link =>
     link.match(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?=\.ics)/g);
 window.addEventListener('load', () => {
-    Array.from(document.getElementsByClassName('needs-validation')).forEach(form => {
+    Array.from(document.getElementsByClassName('needs-validation')).forEach(form =>
         form.addEventListener(
             'submit',
             event => {
                 event.preventDefault();
                 event.stopPropagation();
-                if (form.checkValidity()) $('#disclaimerModal').modal();
+                if (!form.checkValidity()) {
+                    document.getElementById('allClassesUrl').setAttribute('class', 'input is-danger');
+                    document.getElementById('invalid').style.display = '';
+                } else {
+                    document.getElementById('allClassesUrl').setAttribute('class', 'input is-success');
+                    document.getElementById('invalid').style.display = 'none';
+                    document.getElementById('disclaimerModal').setAttribute('class', 'modal is-active is-clipped');
+                    Array.from(document.getElementsByClassName('close-modal')).forEach(el =>
+                        el.addEventListener('click', () =>
+                            document.getElementById('disclaimerModal').setAttribute('class', 'modal')
+                        )
+                    );
+                }
                 form.classList.add('was-validated');
             },
             false
-        );
-    });
+        )
+    );
     document.getElementsByClassName('generate-link')[0].addEventListener('click', () => {
         localStorage.removeItem('scheduleEvents');
         window.open(
