010e22aee81c657986abfa1e67d2308273e626e4
Liam Wang <5054950+wangl5@users.noreply.github.com>
2019-10-14 10:17:46 -0700

    Add message if zero blocks are identified

--- src/schedule/veracross/veracrossICSRawBlockSource.ts
+++ src/schedule/veracross/veracrossICSRawBlockSource.ts
@@ -12,27 +12,38 @@ export class VeracrossICSRawBlockSource implements RawBlockSource {
 
     getBlocksPromise(): Promise<RawBlock[]> {
         return VeracrossICalUtils.getVeracrossCalendarFromUUID(this._calendarUUID).then(calendarEvents => {
-            return calendarEvents
+            let filteredBlocks = calendarEvents
                 .map((event: any) => {
-                    let date = VeracrossICalUtils.getDate(event[1]);
-                    let startTime = VeracrossICalUtils.getStartTime(event[1]);
-                    let endTime = VeracrossICalUtils.getEndTime(event[1]);
-                    let title = VeracrossICalUtils.getTitle(event[1]);
-                    let location = VeracrossICalUtils.getLocation(event[1]);
-                    let letter = VeracrossICalUtils.getLetter(event[1]);
-                    let label = VeracrossICalUtils.getLabel(event[1]);
+                    try {
+                        let date = VeracrossICalUtils.getDate(event[1]);
+                        let startTime = VeracrossICalUtils.getStartTime(event[1]);
+                        let endTime = VeracrossICalUtils.getEndTime(event[1]);
+                        let title = VeracrossICalUtils.getTitle(event[1]);
+                        let location = VeracrossICalUtils.getLocation(event[1]);
+                        let letter = VeracrossICalUtils.getLetter(event[1]);
+                        let label = VeracrossICalUtils.getLabel(event[1]);
 
-                    if (
-                        date === null ||
-                        endTime === null ||
-                        startTime === null ||
-                        title.match(/Morning Choir/) !== null // TODO: Remove custom rules
-                    )
+                        if (
+                            date === null ||
+                            endTime === null ||
+                            startTime === null ||
+                            title.match(/Morning Choir/) !== null // TODO: Remove custom rules
+                        )
+                            return null;
+
+                        return new RawBlock(title, location, label, date, new ScheduleDayMeta(letter), startTime, endTime);
+                    } catch (e) {
                         return null;
-
-                    return new RawBlock(title, location, label, date, new ScheduleDayMeta(letter), startTime, endTime);
+                    }
                 })
                 .filter((rawBlock: any) => rawBlock !== null);
+            if (filteredBlocks.length === 0) {
+                let message = 'Unable to read schedule from the provided calendar link! Make sure to copy your calendar link from the correct \'Subscribe\' button in step 2!';
+                alert(message);
+                window.location.href = './index.html'; // exits the page
+                throw new Error(message);
+            }
+            return filteredBlocks;
         });
     }
 }
